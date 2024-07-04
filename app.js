ccce39d5ed350455b7ad9df99a6509d940a2c0b2
Socialfew146575 <95147819+Socialfew146575@users.noreply.github.com>
2024-07-04 16:22:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,6 +49,8 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
+app.set('views', path.join(__dirname, 'views'));
+
 // Routes
 
 app.use("/", notes);
